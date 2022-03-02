c338ff2045ef9bc1f26f72e92b30bb2291cb2966
Ayush <88432311+Ayush-Pujari@users.noreply.github.com>
2022-03-02 18:28:56 +0530

    Add files via upload

--- Project89.js
+++ Project89.js
@@ -1,1 +1,9 @@
- 
+function addUser(){
+    var player1_name=document.getElementById("player1_name_input").value;
+    var player2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1" ,player1_name);
+    localStorage.setItem("player2" ,player2_name);
+    window.location="output.html";
+}
+player1_score = 0;
+player2_score = 0;
