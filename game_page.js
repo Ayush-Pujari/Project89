24bab6930d6dc0ed707a8f9583631a5b9a39efe1
Ayush <88432311+Ayush-Pujari@users.noreply.github.com>
2022-03-25 18:44:59 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -2,7 +2,7 @@ var player1_name=localStorage.getItem("player1");
 var player2_name=localStorage.getItem("player2");
 
 var player1_score=0;
-var player2_score=0;
+var player2_score=0; 
 document.getElementById("player1_name").innerHTML=player1_name+" : ";
 document.getElementById("player2_name").innerHTML=player2_name+" : ";
 document.getElementById("player1_score").innerHTML=player1_score;
